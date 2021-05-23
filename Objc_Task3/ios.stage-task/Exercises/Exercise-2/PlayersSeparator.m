7ec7268f5e1b8cca67513c6b5cd30741cc6f0556
Olga Berezka <koltsovaolga5@gmail.com>
2021-05-23 16:55:11 +0300

    feat: add Ex. 2 and Ex. 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,24 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    NSInteger count = 0;
+    NSInteger n = ratingArray.count;
+    
+    if (n < 3) {
+        return 0;
+    }
+    
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            for (int k = j + 1; k < n; k++)
+                if (ratingArray[i].intValue < ratingArray[j].intValue &&
+                    ratingArray[j].intValue < ratingArray[k].intValue)
+                    count++;
+                else if (ratingArray[i].intValue > ratingArray[j].intValue &&
+                        ratingArray[j].intValue > ratingArray[k].intValue)
+                        count++;
+    return count;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,32 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSString *firstString = [[NSString alloc] initWithString: string1];
+    NSString *secondString = [[NSString alloc] initWithString: string2];
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+   
+   while ([firstString length] && [secondString length]) {
+       
+       const char *first = [firstString UTF8String];
+       const char *second = [secondString UTF8String];
+       
+       if (first[0] < second[0] || first[0] == second[0]) {
+           [resultString appendString: [firstString substringToIndex: 1]];
+           firstString = [firstString substringFromIndex: 1];
+       } else {
+           [resultString appendString: [secondString substringToIndex: 1]];
+           secondString = [secondString substringFromIndex: 1];
+       }
+   }
+   
+   if ([firstString length]) {
+       [resultString appendString: firstString];
+   } else {
+       [resultString appendString: secondString];
+   }
+   
+   return resultString;
 }
 
 @end
